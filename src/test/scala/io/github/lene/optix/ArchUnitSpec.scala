23cf0441223bf74ef2d0332ca41d75a601690b99
Lene Preuss <lene.preuss@gmail.com>
2026-06-30 22:09:14 +0000

    test: restore ArchUnit circular-dep check

--- src/test/scala/io/github/lene/optix/ArchUnitSpec.scala
+++ src/test/scala/io/github/lene/optix/ArchUnitSpec.scala
@@ -4,17 +4,20 @@ import com.tngtech.archunit.core.importer.ClassFileImporter
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices
 import org.scalatest.flatspec.AnyFlatSpec
 
-class ArchUnitSpec extends AnyFlatSpec {
+/** Module-scoped ArchUnit rules (T5, Sprint 32).
+  *
+  * Fitness functions:
+  * - No circular dependencies in optix-jni package structure
+  */
+class ArchUnitSpec extends AnyFlatSpec:
 
   private val classes = new ClassFileImporter()
     .importPackages("io.github.lene.optix")
 
-  "Package structure" should "be free of circular dependencies" in {
+  "Package structure" should "be free of circular dependencies" in:
     slices()
       .matching("io.github.lene.optix.(*)..")
       .should()
       .beFreeOfCycles()
       .allowEmptyShould(true)
       .check(classes)
-  }
-}
